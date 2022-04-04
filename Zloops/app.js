734052128545a40f07d78a0812bf1bdbb6584ee5
Zeshan khan <98458839+zeeshankhank@users.noreply.github.com>
2022-04-04 17:45:28 +0500

    Add files via upload

---
+++ Zloops/app.js
@@ -0,0 +1,163 @@
+// Chapter no #1 //
+
+
+
+//Question no #3 //
+
+// for (var i = 1; i <= 10; i++) {
+//   document.write( "<b>" +  i + "</br>");
+// }
+
+
+
+    // Question no #4 //
+
+// var tableN = +prompt("enter table N");
+// var tableL = +prompt("enter table L");
+
+// if (tableN = 2) {
+//   tableN = tableN;
+// } else {
+//   tableN = 2;
+// }
+
+// if (tableL) {
+//   tableL = tableL;
+// } else {
+//   tableL = 15;
+// }
+
+// for (var i = 1; i <= tableL; i++) {
+//   document.write(
+//     tableN + "" + "x" + " " + i + " = " + tableN * i + "<br />"
+//   );
+// }
+// Question no #5 //
+
+// var fruits = new Array("apple", "banana", "mango", "orange",
+//     "strawberry");
+
+// var index = fruits.length;
+
+// for (var i = 0; i <=index; i++) {
+// 	console.log(fruits[i]);
+// }
+
+// Question no #6
+
+// document.write("<b>"+"counting" + "</br></br>")
+// for (var i = 1; i <= 15; i++) {
+//   document.write(  "<b>"+ i +",  "  );
+// }
+// document.write("</br></br>"+"<b>"+"Reverse counting: " + "</br></br>")
+
+// for (var i = 10; i >= 1; i--) {
+//   document.write(  "<b>"+ i +",  "  );
+// }
+// document.write("</br></br>"+"<b>"+"even: " + "</br></br>")
+
+// for (var i = 0; i <= 20; i = i + 2) {
+//   document.write(  "<b>"+ i +",  "  );
+// }
+
+// document.write("</br></br>"+"<b>"+"odd: " + "</br></br>")
+
+// for (var i = 1; i <= 19; i = i + 2) {
+//     document.write(  "<b>"+ i +",  "  );
+// }
+// document.write("</br></br>"+"<b>"+"Series: " + "</br></br>")
+
+// for (var i = 2; i <= 20; i = i + 2) {
+//     document.write( "<b>"+ i +  "k" +","  );
+
+//   }
+// Question no #7 //
+
+// Question no #8 //
+
+
+// var array = [24, 53, 78, 91, 12];
+// var largest= 91;
+
+// for (i=0; i<=largest;i++){
+//     if (array>largest) {
+//         var largest=array[i];
+//     }
+// }
+
+// console.log(largest);
+
+
+// Question no #9 //
+
+
+// var array = [24, 53, 78, 91, 12];
+// var smallest= 12;
+
+// for (i=0; i<=smallest;i++){
+//     if (array>smallest) {
+//         var smallest=array[i];
+//     }
+// }
+
+// console.log(smallest);
+
+
+//  Question no #10 //
+
+// for (var i = 5; i <= 100; i =i+ 5  ) {
+//     document.write( "<b>" + i  + ", "  );
+//   }
+
+
+// Chapter no #2 //
+
+
+
+// Question no #7 //
+
+
+
+// Question no #8 //
+
+
+// var stdName = ["Micheal", "Jhon", "Tony"];
+// var stdScore = [320, 230, 480];
+
+// var per1 = (stdScore[0] / 500) * 100;
+// var per2 = (stdScore[1] / 500) * 100;
+// var per3 = (stdScore[2] / 500) * 100;
+
+// document.write ("<b> score of "  + stdName[0] + " is " + stdScore[0]  + " percentage: " + per1 +"%" +  "<br>"); 
+// document.write (" score of "  + stdName[1] + " is " + stdScore[1]  + " percentage: " + per2 +"%" + "<br>");
+// document.write (" score of "  + stdName[2] + " is " + stdScore[2]  + " percentage: " + per3 +"%");
+
+// Question no #9 //
+
+// Question no #10 //
+
+
+// var number = ["320" , "230", "480" , "120"];
+
+// document.write( "<h1>hello shani</h1>" + " <b> Scores of students : " + number  + "<br>")
+
+// document.write("odered Scores Of Students :" + number)
+
+// Question no #11 //
+
+// var cities = ["Karachi", "Lahore","Islamabad", "Quetta", "Peshawar"];
+
+//  document.write("<b>cities list <br>" + cities + "<br> <br> <br>")
+// var cities = cities.slice(2, 4);
+
+// document.write("selected cities list <br>"  + cities)
+
+// Question no #12 //
+
+
+// var a = [ "This,", "is,", "my," , "cat," ]; 
+//       document.write("Array: <br><br>" + a.join("a" )) ;
+
+//       "<br>"
+//     var b = [ "This is my cat" + "<br>" ]; 
+//       document.write("<br>String <br>"  +(b));
